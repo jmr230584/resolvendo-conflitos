92f08543719cc5045c5fcb77bc543a506094f1c4
Prof. José GIT <prof.jose.git@gmail.com>
2025-03-02 18:19:32 -0300

    alunoB inserindo linha6 vazia e linha 7 com código

--- conflito.ts
+++ conflito.ts
@@ -4,8 +4,5 @@ console.log(`alunoB inserindo a linha 2`);
 let calculo: number = (75 + 75) /3; //imaginando um calculo qualquer
 let concatenar: string = `Git` + `Hub`;
 console.log(`Concatenado ${concatenar}`);
-<<<<<<< HEAD
-=======
 
 console.log(`Inserindo linha 7 pelo alunoB`);
->>>>>>> f7978ba77e76eb2a4f229b2d2f48f2510916bd61
